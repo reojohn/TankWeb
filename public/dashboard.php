fbc5dca68f814d4c8d5a0fed0565548260dd08b6
Reo John <randohuyan@gmail.com>
2025-11-23 19:48:54 +0700

    Enable error display for debugging

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,5 +1,8 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require __DIR__ . '/../src/middleware.php';
 require_once __DIR__ . '/../src/auth.php';
 require_once __DIR__ . '/../src/logger.php';
