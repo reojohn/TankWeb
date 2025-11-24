1e5a0132c2f1eeaed9446cf69a31a6e6a93f2527
Reo John <randohuyan@gmail.com>
2025-11-24 08:32:58 +0700

    Fix: Enable error reporting and add database config

--- src/config.php
+++ src/config.php
@@ -4,9 +4,11 @@
 // ---------------------------------
 // Production Error Handling
 // ---------------------------------
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
-error_reporting(0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 
 ini_set('log_errors', 1);
 ini_set('error_log', __DIR__ . '/../logs/errors.log');
